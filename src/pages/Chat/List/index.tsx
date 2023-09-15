3ca3bc91122bdbbcfcc340527127ec7b26189680
namelew <diogomaciel.cunha@gmail.com>
2023-09-15 17:16:36 -0300

    Adicionando botão e base do modal de cadastro de nova conversa

--- src/pages/Chat/List/index.tsx
+++ src/pages/Chat/List/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, TextField, List, ListItem, ListItemText } from '@mui/material';
+import { Box, TextField, List, ListItem, ListItemText, Button, Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material';
 import { IConversation } from 'interfaces/IConversation';
 
 interface Props {
@@ -9,10 +9,19 @@ interface Props {
 const ConversationsList: React.FC<Props> = ( { selectConversation } : Props ) => {
   const [conversations, setConversations] = useState<IConversation[]>([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [openModal, setOpenModal] = useState(false);
 
   const filteredConversations = conversations.filter((conversation) =>
     conversation.name.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  const handleOpenModal = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
 
   return (
     <Box
@@ -24,11 +33,22 @@ const ConversationsList: React.FC<Props> = ( { selectConversation } : Props ) =>
     >
       <TextField
         variant="outlined"
-        placeholder="Search"
+        placeholder="Pesquisar"
         fullWidth
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
+      <Button onClick={handleOpenModal}>Nova conversa</Button>
+      <Dialog open={openModal} onClose={handleCloseModal}>
+        <DialogTitle>Informe as informações do canal</DialogTitle>
+        <DialogContent>
+          {/* Add your form for creating a new conversation here */}
+        </DialogContent>
+        <DialogActions>
+          <Button color='error' onClick={handleCloseModal}>Cancel</Button>
+          <Button onClick={handleCloseModal}>Create</Button>
+        </DialogActions>
+      </Dialog>
       <List
         sx={{
           flexGrow: 1,
