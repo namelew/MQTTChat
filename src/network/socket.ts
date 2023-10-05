4c4d8aab6ab3258ea410faf95609eb9c4beff300
namelew <diogomaciel.cunha@gmail.com>
2023-10-05 12:04:47 -0300

    Adicionando web socket para conexão com o servidor

---
+++ src/network/socket.ts
@@ -0,0 +1,25 @@
+import { IMessageType } from 'interfaces/IMessage';
+import { useLocation, useParams } from 'react-router-dom';
+import { WebSocket } from 'ws';
+
+
+const socket = new WebSocket('ws://localhost:8000');
+
+socket.on('open', () => {
+    const { state } = useLocation();
+
+    if (state.clientID) {
+        socket.send(`{
+            id: ${Date.now()},
+            type: ${IMessageType.Request},
+            sender: {
+                id: ${state.clientID},
+                name: ${state.clientID},
+            },
+            timestamp: ${new Date()},
+            payload: open,
+        }`);
+    }
+});
+
+export default socket;
