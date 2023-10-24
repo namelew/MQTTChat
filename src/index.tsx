fc3e79066e0d1217170f6c5f14f2dc044beda73d
namelew <diogomaciel.cunha@gmail.com>
2023-10-24 07:57:00 -0300

    Removing React Query call in index file

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import AppRouter from 'router';
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-        <AppRouter />
-    </QueryClientProvider>
+      <AppRouter />
   </React.StrictMode>
 );
